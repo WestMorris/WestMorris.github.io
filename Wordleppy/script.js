094c790c4d11f00415afffe636e4e858167b1c49
WestMorris <47751520+WestMorris@users.noreply.github.com>
2023-12-04 11:22:39 -0300

    Wordle funcional
    Pagina del juego

---
+++ Wordleppy/script.js
@@ -0,0 +1,106 @@
+const button = document.getElementById("guess-button");
+
+const input = document.getElementById("guess-input");
+const valor = input.value;
+
+
+let intentos = 6;
+let diccionario = ['APPLE', 'HURLS', 'WINGS', 'YOUTH']
+const palabra = diccionario[Math.floor(Math.random() * diccionario.length)];
+
+window.addEventListener('load', init)
+
+function init(){
+    console.log('Esto se ejecuta solo cuando se carga la pagina web')
+}
+
+button.addEventListener("click", intentar);
+input.addEventListener('keypress', function (event) {
+    if (event.key === 'Enter') {
+        intentar();
+        event.preventDefault();
+    }
+});
+
+function intentar(){
+    const INTENTO = leerIntento();
+    if (INTENTO === palabra ) {
+        terminar("<h1>GANASTE!😀</h1>");
+        
+    }
+    //----------------
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#79b851';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#f3c237';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#a4aec4';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+
+    //----------------
+    /*
+    for (let i in palabra){
+        if (INTENTO[i]===palabra[i]){
+            console.log(INTENTO[i], "VERDE")
+        } else if( palabra.includes(INTENTO[i]) ) {
+            console.log(INTENTO[i], "AMARILLO")
+        } else {
+            console.log(INTENTO[i], "GRIS")
+        }
+    }
+    */
+		intentos--
+    if (intentos==0){
+        terminar("<h1>PERDISTE!😖</h1>");
+    }
+}
+
+function matrizFinal(INTENTO) {
+    const GRID = document.getElementById("grid");
+    const ROW = document.createElement('div');
+    ROW.className = 'row';
+    for (let i in palabra){
+        const SPAN = document.createElement('span');
+        SPAN.className = 'letter';
+        if (INTENTO[i]===palabra[i]){ //VERDE
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#79b851';
+        } else if( palabra.includes(INTENTO[i]) ) { //AMARILLO
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#f3c237';
+        } else {      //GRIS
+            SPAN.innerHTML = INTENTO[i];
+            SPAN.style.backgroundColor = '#a4aec4';
+        }
+        ROW.appendChild(SPAN)
+    }
+    GRID.appendChild(ROW)
+}
+
+function leerIntento(){
+    let intento = document.getElementById("guess-input");
+    intento = intento.value;
+    intento = intento.toUpperCase(); 
+    return intento;
+}
+
+function terminar(mensaje){
+    const INPUT = document.getElementById("guess-input");
+    INPUT.disabled = true;
+    button.disabled = true;
+    let contenedor = document.getElementById('guesses');
+    contenedor.innerHTML = mensaje;
+}
+
